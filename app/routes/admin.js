d99eb839398e8f3bc8aaacf0fed0a997de85a9b0
Valter Neto <valter.neto32@gmail.com>
2017-08-30 17:29:58 +0000

    salvando noticia, implementando mensagens de erro

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,23 +1,23 @@
 module.exports = (app) =>{
   app.get('/form_inclusao_noticia', (req, res)=>{
-    res.render('admin/form_add_noticia');
+    res.render('admin/form_add_noticia', { validacao : {}, noticia : {} });
   });
   
   app.post('/noticias/salvar',(req, res)=>{
     const noticia = req.body;
     
-    req.assert('titulo','titulo é obrigatório!').notEmpty();
-    req.assert('resumo','resumo é obrigatório!').notEmpty();
-    req.assert('resumo','resumo deve conter entre 10 e 100 caracteres!').len(10, 100);
-    req.assert('autor','autor é obrigatório!').notEmpty();
-    req.assert('data_noticia','data é obrigatório!').notEmpty().isDate({format: 'YYYY-MM-DD'});
-    req.assert('noticia','noticia é obrigatório!').notEmpty();
+    req.assert('titulo', 'Título é obrigatório').notEmpty();
+    req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+    req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+    req.assert('autor', 'Autor é obrigatório').notEmpty();
+    req.assert('data_noticia', 'Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+    req.assert('noticia', 'Notícia é obrigatório').notEmpty();
     
-    const errors = req.validationErrors();
+    var erros = req.validationErrors();
     
-    if(errors){
-      res.render("admin/form_add_noticia", {validacao: errors});
-      return;
+    if(erros){
+        res.render('admin/form_add_noticia', {validacao: erros, noticia: noticia});
+        return;
     }
     
     const connection = app.config.dbConnection();
